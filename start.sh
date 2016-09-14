a9c83893ee76d6737f3145cb66fbc4f174a25949
Tom Whitwell <tnwhitwell@gmail.com>
2016-09-14 13:00:12 +0100

    changed location - app is now on a mapped volume. don't delete it.

--- start.sh
+++ start.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
 while ! nc -z $MYSQL_HOST 3306; do sleep 3; done
-cd /data
 if [ ! -d /data/ccnet ]
 then
-    rm -rf /conf
-    rm -rf /ccnet
-    bash /seafile/setup-seafile-mysql.sh auto -d /data/seadata -q $(hostname -i)
-    sed -i.bak 's/email = ask_admin_email()/email = os.environ["SEA_ADMIN_USER"]/' /seafile/check_init_admin.py
-    sed -i.bak 's/passwd = ask_admin_password()/passwd = os.environ["SEA_ADMIN_PASS"]/' /seafile/check_init_admin.py
-    echo "FILE_SERVER_ROOT = http" >> /conf/seahub_settings.py
-    mv /conf /data/
-    mv /ccnet /data/
+    mv /seafile /data
+    bash /data/seafile/setup-seafile-mysql.sh auto -d /data/seadata -q $(hostname -i)
+    sed -i.bak 's/email = ask_admin_email()/email = os.environ["SEA_ADMIN_USER"]/' /data/seafile/check_init_admin.py
+    sed -i.bak 's/passwd = ask_admin_password()/passwd = os.environ["SEA_ADMIN_PASS"]/' /data/seafile/check_init_admin.py
+    echo "FILE_SERVER_ROOT = http" >> /data/conf/seahub_settings.py
 fi
-ln -s /data/conf /conf
-ln -s /data/ccnet /ccnet
 
 echo "Downloading new Caddyserver"
 wget -O /tmp/caddy.tar.gz "https://caddyserver.com/download/build?os=linux&arch=amd64&features=cors%2Cfilemanager%2Cgit%2Chugo%2Cipfilter%2Cjwt%2Clocale%2Cmailout%2Cminify%2Cprometheus%2Cratelimit%2Crealip%2Csearch%2Cupload%2Ccloudflare%2Cdigitalocean%2Cdnsimple%2Cdyn%2Cgandi%2Cgooglecloud%2Cnamecheap%2Crfc2136%2Croute53%2Cvultr"
@@ -27,20 +21,13 @@ echo "$WEB_FQDN {
     # fileserver
     #
     fastcgi / 127.0.0.1:8000
-# {
-#         header_upstream Host {host}
-#         header_upstream X-Real-IP {remote}
-#         header_upstream X-Forwarded-For {remote}
-#         header_upstream X-Forwarded-Proto {scheme}
-#         max_fails 0
-#    }
     gzip
     log stdout
     errors stdout
 }
 
 $WEB_FQDN/media {
-    root /seafile/seahub/media/
+    root /data/seafile/seahub/media/
     gzip
     log stdout
     errors stdout
@@ -67,10 +54,10 @@ $WEB_FQDN/seafhttp {
 }"> /etc/Caddyfile
 
 
-sed -i "/^SERVICE_URL = /s/= .*/= https:\/\/$WEB_FQDN/" /conf/ccnet.conf
-sed -i "/^FILE_SERVER_ROOT = /s/= .*/= 'https:\/\/$WEB_FQDN\/seafhttp'/" /conf/seahub_settings.py
+sed -i "/^SERVICE_URL = /s/= .*/= https:\/\/$WEB_FQDN/" /data/conf/ccnet.conf
+sed -i "/^FILE_SERVER_ROOT = /s/= .*/= 'https:\/\/$WEB_FQDN\/seafhttp'/" /data/conf/seahub_settings.py
 
-/seafile/seafile.sh start
-/seafile/seahub.sh start-fastcgi
+/data/seafile/seafile.sh start
+/data/seafile/seahub.sh start-fastcgi
 
 caddy --conf /etc/Caddyfile
